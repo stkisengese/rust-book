46310dbec81622d4b7c662cab5d3d487e8b82579
stkisengese <skisengese@outlook.com>
2025-04-02 11:37:52 +0300

    feat: remove options as result

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -1,27 +1,19 @@
-pub fn sum(a: u8, b: u8) -> Option<u8> {
-    a.checked_add(b)
+pub fn sum(a: u8, b: u8) -> u8 {
+    a + b
 }
 
-pub fn diff(a: i16, b: i16) -> Option<i16> {
-    a.checked_sub(b)
+pub fn diff(a: i16, b: i16) -> i16 {
+    a - b
 }
 
-pub fn pro(a: i8, b: i8) -> Option<i8> {
-    a.checked_mul(b)
+pub fn pro(a: i8, b: i8) -> i8 {
+    a * b
 }
 
-pub fn quo(a: f32, b: f32) -> Option<f32> {
-    if b == 0.0 {
-        None
-    } else {
-        Some(a / b)
-    }
+pub fn quo(a: f32, b: f32) -> f32 {
+    a / b
 }
 
-pub fn rem(a: f32, b: f32) -> Option<f32> {
-    if b == 0.0 {
-        None
-    } else {
-        Some(a % b)
-    }
+pub fn rem(a: f32, b: f32) -> f32 {
+    a % b
 }
