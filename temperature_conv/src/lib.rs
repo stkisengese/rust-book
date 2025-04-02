9d783713d7bb7ebea090aeb0c80ff7dcd3f87338
stkisengese <skisengese@outlook.com>
2025-04-02 12:16:55 +0300

    feat: add tests

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,7 +1,3 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
     (f - 32.0) * 5.0 / 9.0
 }
@@ -15,8 +11,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn c_to_f() {
+        let result = celsius_to_fahrenheit(0.0);
+        assert_eq!(result, 32.0);
+    }
+    #[test]
+    fn f_to_c() {
+        let result = fahrenheit_to_celsius(20.0);
+        assert_eq!(result, -6.666666666666667);
     }
 }
