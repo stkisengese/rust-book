8da43224d21c54ed9e0a6f6cddbad52c4bd9d89a
stkisengese <skisengese@outlook.com>
2025-04-02 11:55:36 +0300

    feat: initial commit

---
+++ temperature_conv/src/lib.rs
@@ -0,0 +1,22 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+pub fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - 32.0) * 5.0 / 9.0
+}
+
+pub fn celsius_to_fahrenheit(c: f64) -> f64 {
+    (c * 9.0 / 5.0) + 32.0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
