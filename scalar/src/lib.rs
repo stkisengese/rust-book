9573d26e6a99e62adf7e840ee7b20b9b3a1c85f6
stkisengese <skisengese@outlook.com>
2025-04-02 11:34:58 +0300

    fix: fix result type

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -1,81 +1,27 @@
-use std::fmt;
-
-#[derive(Debug, PartialEq)]
-pub enum ScalarError {
-    Overflow,
-    DivisionByZero,
+pub fn sum(a: u8, b: u8) -> Option<u8> {
+    a.checked_add(b)
 }
 
-impl fmt::Display for ScalarError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            ScalarError::Overflow => write!(f, "ERROR: attempt to add with overflow"),
-            ScalarError::DivisionByZero => write!(f, "ERROR: attempt to divide by zero"),
-        }
-    }
+pub fn diff(a: i16, b: i16) -> Option<i16> {
+    a.checked_sub(b)
 }
 
-pub fn sum(a: u8, b: u8) -> Result<u8, ScalarError> {
-    a.checked_add(b).ok_or(ScalarError::Overflow)
+pub fn pro(a: i8, b: i8) -> Option<i8> {
+    a.checked_mul(b)
 }
 
-pub fn diff(a: i16, b: i16) -> Result<i16, ScalarError> {
-    a.checked_sub(b).ok_or(ScalarError::Overflow)
-}
-
-pub fn pro(a: i8, b: i8) -> Result<i8, ScalarError> {
-    a.checked_mul(b).ok_or(ScalarError::Overflow)
-}
-
-pub fn quo(a: f32, b: f32) -> Result<f32, ScalarError> {
+pub fn quo(a: f32, b: f32) -> Option<f32> {
     if b == 0.0 {
-        Err(ScalarError::DivisionByZero)
+        None
     } else {
-        Ok(a / b)
+        Some(a / b)
     }
 }
 
-pub fn rem(a: f32, b: f32) -> Result<f32, ScalarError> {
+pub fn rem(a: f32, b: f32) -> Option<f32> {
     if b == 0.0 {
-        Err(ScalarError::DivisionByZero)
+        None
     } else {
-        Ok(a % b)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn sum_test() {
-        assert_eq!(sum(254, 2), Err(ScalarError::Overflow));
-        assert_eq!(sum(1, 2), Ok(3));
-    }
-
-    #[test]
-    fn diff_test() {
-        assert_eq!(diff(234, 2), Ok(232));
-        assert_eq!(diff(-32768, 32766), Err(ScalarError::Overflow));
-    }
-
-    #[test]
-    fn pro_test() {
-        assert_eq!(pro(23, 2), Ok(46));
-        assert_eq!(pro(-128, 2), Err(ScalarError::Overflow));
-    }
-
-    #[test]
-    fn quo_test() {
-        assert_eq!(quo(22.0, 2.0), Ok(11.0));
-        assert_eq!(quo(-128.23, 2.0), Ok(-64.115));
-        assert_eq!(quo(22.0, 0.0), Err(ScalarError::DivisionByZero));
-    }
-
-    #[test]
-    fn rem_test() {
-        assert_eq!(rem(22.0, 2.0), Ok(0.0));
-        assert_eq!(rem(-128.23, 2.0), Ok(-0.22999573));
-        assert_eq!(rem(22.0, 0.0), Err(ScalarError::DivisionByZero));
+        Some(a % b)
     }
 }
