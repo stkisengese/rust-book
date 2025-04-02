9fac0b3a94116b276a9d244304ace5a9504ab47d
stkisengese <skisengese@outlook.com>
2025-04-02 12:25:18 +0300

    feat: remove tests

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -6,18 +6,3 @@ pub fn celsius_to_fahrenheit(c: f64) -> f64 {
     (c * (9.0 / 5.0)) + 32.0
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn c_to_f() {
-        let result = celsius_to_fahrenheit(0.0);
-        assert_eq!(result, 32.0);
-    }
-    #[test]
-    fn f_to_c() {
-        let result = fahrenheit_to_celsius(20.0);
-        assert_eq!(result, -6.666666666666667);
-    }
-}
