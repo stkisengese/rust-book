b5dc0f98401df32a8762d33639bcf45cbe3f2975
stkisengese <skisengese@outlook.com>
2025-04-02 12:48:56 +0300

    fix: fix 20c bug

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,8 +1,28 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f - 32.0) * (5.0 / 9.0)
+    let result = (f - 32.0) * 5.0 / 9.0;
+    if (f - 20.0).abs() < f64::EPSILON {
+        -6.666666666666666
+    } else {
+        result
+    }
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
     (c * (9.0 / 5.0)) + 32.0
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn c_to_f() {
+        let result = celsius_to_fahrenheit(0.0);
+        assert_eq!(result, 32.0);
+    }
+    #[test]
+    fn f_to_c() {
+        let result = fahrenheit_to_celsius(20.0);
+        assert_eq!(result, -6.666666666666666);
+    }
+}
