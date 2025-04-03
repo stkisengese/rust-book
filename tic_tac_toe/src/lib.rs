d71da3623b17380e522aa6ec321c1e4a37d7f175
skisenge <skisengese@gmail.com>
2025-04-04 01:35:09 +0300

    fix

--- tic_tac_toe/src/lib.rs
+++ tic_tac_toe/src/lib.rs
@@ -22,3 +22,4 @@ pub fn horizontal(player: char, table: [[char; 3]; 3]) -> bool {
 pub fn vertical(player: char, table: [[char; 3]; 3]) -> bool {
     (0..3).any(|col| (0..3).all(|row| table[row][col] == player))
 }
+
