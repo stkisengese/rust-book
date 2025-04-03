121a367720a32333972eae7149f218743235e72f
skisenge <skisengese@gmail.com>
2025-04-03 16:44:30 +0300

    feat: add manul s.replace() implementation

--- to_url/src/lib.rs
+++ to_url/src/lib.rs
@@ -1,5 +1,17 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// To do
+// Create a function named to_url which takes a 
+// string and substitutes every ASCII space with "%20".
+
+pub fn to_url(s: &str) -> String {
+    let mut result = String::new();
+    for c in s.chars() {
+        if c == ' ' {
+            result.push_str("%20");
+        } else {
+            result.push(c);
+        }
+    }
+    result
 }
 
 #[cfg(test)]
@@ -8,7 +20,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = to_url("Hello World");
+        assert_eq!(result, "Hello%20World");
     }
 }
