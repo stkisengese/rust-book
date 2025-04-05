47c8132c71be621885176d45f50dbfd3ed437327
skisenge <skisengese@gmail.com>
2025-04-05 21:04:54 +0300

    feat: add recusion to edit distance

--- edit_distance/src/lib.rs
+++ edit_distance/src/lib.rs
@@ -1,5 +1,19 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// Create a function named edit_distance, which calculates the minimum 
+// number of changes (insertions, deletions and/or substitutions) which
+// are needed to transform the source string to the target string.
+
+pub fn edit_distance(source: &str, target: &str) -> usize {
+    if source.is_empty() { return target.len(); }
+    if target.is_empty() { return source.len(); }
+
+    let (s_head, s_tail) = source.split_at(1);
+    let (t_head, t_tail) = target.split_at(1);
+
+    if s_head == t_head {
+        edit_distance(s_tail, t_tail)
+    } else {
+        1 + edit_distance(source, t_tail).min(edit_distance(s_tail, target)).min(edit_distance(s_tail, t_tail))
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +21,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_edit_distance() {
+        let result = edit_distance("alignment", "assignment");
+        assert_eq!(result, 2);
+    }
+    #[test]
+    fn test_edit_distance2() {
+        let result = edit_distance("sitting", "kitten");
+        assert_eq!(result, 3);
     }
 }
