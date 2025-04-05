c9305894ad9ad3262e81046347fa74482e6556aa
skisenge <skisengese@gmail.com>
2025-04-05 18:49:58 +0300

    feat: add capitalize methods

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -1,5 +1,37 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// capitalize_first function which converts the 
+// first letter of the string to uppercase.
+pub fn capitalize_first(input: &str) -> String {
+    let mut chars = input.chars();
+    match chars.next() {
+        None => String::new(),
+        Some(first) => first.to_uppercase().chain(chars).collect(),
+    }
+}
+
+
+// title_case function which converts the 
+// first letter of each word in a string to uppercase.
+
+pub fn title_case(input: &str) -> String {
+    input
+        .split_whitespace()
+        .map(|word| capitalize_first(word))
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+// change_case function which converts all uppercase letters to lowercase and vice versa.
+pub fn change_case(input: &str) -> String {
+    input
+        .chars()
+        .map(|c| {
+            if c.is_uppercase() {
+                c.to_lowercase().to_string()
+            } else {
+                c.to_uppercase().to_string()
+            }
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -7,8 +39,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_capitalize_first() {
+        let result = capitalize_first("hello");
+        assert_eq!(result, "Hello");
+    }
+
+    #[test]
+    fn test_title_case() {
+        let result = title_case("hello world");
+        assert_eq!(result, "Hello World");
+    }
+
+    #[test]
+    fn test_change_case() {
+        let result = change_case("hElLo wOrLd");
+        assert_eq!(result, "HeLlO WoRlD");
     }
 }
