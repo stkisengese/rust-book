d4f0177224d560b302ece2d9a4d4320ec78eff02
skisenge <skisengese@gmail.com>
2025-04-05 03:09:40 +0300

    feat: initial commit

--- circle/src/lib.rs
+++ circle/src/lib.rs
@@ -1,5 +1,40 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::f64::consts::PI;
+
+#[derive(Debug, Clone, Copy)]
+struct Circle {
+    pub center: Point,
+    pub radius: f64,
+}
+
+
+impl Circle {
+    pub fn new(x: f64, y: f64, radius: f64) -> Self {
+        Self {
+            center: Point(x, y),
+            radius,
+        }
+    }
+    pub fn diameter(&self) -> f64 {
+        self.radius * 2.0
+    }
+
+    pub fn area(&self) -> f64 {
+        PI * self.radius.powi(2)
+    }
+    pub fn intersect(&self, other: &Circle) -> bool {
+        self.center.distance(other.center) <= (self.radius + other.radius)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Point(pub f64, pub f64);
+
+impl Point {
+    pub fn distance(&self, other: Point) -> f64 {
+        let dx = self.0 - other.0;
+        let dy = self.1 - other.1;
+        (dx.powi(2) + dy.powi(2)).sqrt()
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +43,17 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let circle = Circle::new(500.0, 500.0, 150.0);
+        let circle1 = Circle {
+            center: Point(80.0, 115.0),
+            radius: 30.0,
+        };
+        let point_a = Point(1.0, 1.0);
+        let point_b = Point(0.0, 0.0);  
+        assert_eq!(circle.area(), 70685.83470577035);
+        assert_eq!(circle.diameter(), 300.0);
+        assert_eq!(circle1.diameter(), 60.0);
+        assert_eq!(circle.intersect(&circle1), false);
+        assert_eq!(point_a.distance(point_b), 1.4142135623730951);
     }
 }
