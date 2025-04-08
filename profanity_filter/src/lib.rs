5779e257cc8814af8bb84d98fd865b6b3e5da280
skisenge <skisengese@gmail.com>
2025-04-09 02:38:24 +0300

    feat initial commit

--- profanity_filter/src/lib.rs
+++ profanity_filter/src/lib.rs
@@ -1,14 +1,26 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub struct Message {
+    content: String,
+    user: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Message {
+    pub fn new(content: String, user: String) -> Self {
+        Self { content, user }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn send_ms(&self) -> Option<&str> {
+        if self.content.is_empty() || self.content.to_lowercase().contains("stupid") {
+            None
+        } else {
+            Some(&self.content)
+        }
+    }
+}
+
+pub fn check_ms(message: &str) -> Result<&str, &str> {
+    let msg = Message::new(message.to_string(), "user".to_string());
+    match msg.send_ms() {
+        Some(content) => Ok(content),
+        None => Err("ERROR: illegal"),
     }
 }
