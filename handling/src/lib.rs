3f55bdee0dc932da5bbf2f896b8ab6082cfe51b2
skisenge <skisengese@gmail.com>
2025-04-08 21:59:21 +0300

    feat: add OpenOptions struct

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::fs::OpenOptions;
+use std::path::Path;
+use std::io::Write;
+// open the file or create file if it doesn't exist
+// append the content to file
+// should panic if error is expected
+pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
+    let mut file = OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open(path)
+                .expect("Failed to open file");
+
+    writeln!(file, "{content}").expect("failed to write");
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
