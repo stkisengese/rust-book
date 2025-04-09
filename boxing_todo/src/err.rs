5c4ba13133de78e67ccf123b78160ffa32246ffc
skisenge <skisengese@gmail.com>
2025-04-09 22:56:56 +0300

    feat: add traits

---
+++ boxing_todo/src/err.rs
@@ -0,0 +1,40 @@
+use std::{error::Error, fmt::Display};
+use std::fmt;
+
+#[derive(Debug)]
+pub enum ParseErr {
+    Empty,
+    Malformed(Box<dyn Error>),
+}
+
+impl Display for ParseErr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Failed to parse todo file")
+    }
+}
+
+impl Error for ParseErr {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            ParseErr::Empty => None,
+            ParseErr::Malformed(e) => Some(e.as_ref()),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ReadErr {
+    pub child_err: Box<dyn Error>,
+}
+
+impl Display for ReadErr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Failed to read todo file")
+    }
+}
+
+impl Error for ReadErr {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(self.child_err.as_ref())
+    }
+}
