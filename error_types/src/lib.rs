2996196d6bb11ec930abb55f99bfbcf8af036ed0
skisenge <skisengese@gmail.com>
2025-04-09 19:21:34 +0300

    fix: fix utc time

--- error_types/src/lib.rs
+++ error_types/src/lib.rs
@@ -1,4 +1,4 @@
-use chrono::Local;
+use chrono::Utc;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct FormError {
@@ -12,7 +12,7 @@ impl FormError {
     pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
         FormError {
             form_values: (field_name, field_value),
-            date: Local::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+            date: Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
             err,
         }
     }
