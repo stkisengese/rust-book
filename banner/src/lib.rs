2ae2c8b7b0e6d9a0a3a07fbe0caef010d1f07b5b
skisenge <skisengese@gmail.com>
2025-04-09 16:16:41 +0300

    feat: add flag opt

--- banner/src/lib.rs
+++ banner/src/lib.rs
@@ -1,14 +1,55 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{collections::HashMap, num::ParseFloatError};
+
+pub struct Flag {
+    short_hand: String,
+    long_hand: String,
+    desc: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Flag {
+    pub fn opt_flag(name: &str, d: &str) -> Self {
+        Flag {
+            short_hand: format!("-{}", name.chars().next().unwrap_or_default()),
+            long_hand: format!("--{}", name),
+            desc: d.to_string(),
+        }
     }
 }
+
+pub type Callback = fn(&str, &str) -> Result<String, ParseFloatError>;
+
+pub struct FlagsHandler {
+    pub flags: HashMap<String, Callback>,
+}
+
+impl FlagsHandler {
+    pub fn add_flag(&mut self, flag: Flag, func: Callback) {
+        self.flags.insert(flag.short_hand.clone(), func);
+        self.flags.insert(flag.long_hand.clone(), func);
+    }
+
+    pub fn exec_func(&self, input: &str, argv: &[&str]) -> Result<String, String> {
+        if argv.len() < 2 {
+            return Err("Not enough arguments".to_string());
+        }
+        match self.flags.get(input) {
+            Some(func) => func(argv[0], argv[1]).map_err(|e| e.to_string()),
+            None => Err("Flag not found".to_string())
+        }
+    }
+}
+
+pub fn div(a: &str, b: &str) -> Result<String, ParseFloatError> {
+    let a_num: f64 = a.parse::<f64>()?;
+    let b_num: f64 = b.parse::<f64>()?;
+
+    Ok((a_num / b_num).to_string())
+}
+
+pub fn rem(a: &str, b: &str) -> Result<String, ParseFloatError> {
+    let a_num: f64 = a.parse::<f64>()?;
+    let b_num: f64 = b.parse::<f64>()?;
+
+    Ok((a_num % b_num).to_string())
+}
+
