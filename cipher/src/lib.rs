c191a7ad06b028cd23fb19b4bc58f2c20384a8e8
skisenge <skisengese@gmail.com>
2025-04-09 17:01:31 +0300

    feat: initial commit

--- cipher/src/lib.rs
+++ cipher/src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+
+#[derive(Debug, PartialEq)]
+pub struct CipherError {
+    pub expected: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn cipher(original: &str, ciphered: &str) -> Result<(), CipherError> {
+    let expected = original.chars()
+    .map(|c| match c {
+        'a'..='z' => (b'z' - (c as u8 - b'a')) as char,
+        'A'..='Z' => (b'Z' - (c as u8 - b'A')) as char,
+        _ => c,
+    })
+    .collect::<String>();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    if ciphered == expected {
+        Ok(())
+    } else {
+        Err(CipherError { expected })
     }
 }
