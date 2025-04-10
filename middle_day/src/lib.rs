05385d073960e1d1ce78792b9d44dcba521d4b23
skisenge <skisengese@gmail.com>
2025-04-10 18:56:51 +0300

    feat: initial commit

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,5 +1,17 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use chrono::{NaiveDate, Datelike, Weekday as wd};
+
+pub fn middle_day(year: i32) -> Option<wd> {
+    if is_leap_year(year) {
+        return None;
+    } 
+    let jan_first = NaiveDate::from_ymd_opt(year, 1, 1)?;
+    let middle_day = jan_first + chrono::Duration::days(182);
+    Some(middle_day.weekday())
+
+}
+
+fn is_leap_year(year: i32) -> bool {
+    NaiveDate::from_ymd_opt(year, 2, 29).is_some()
 }
 
 #[cfg(test)]
@@ -8,7 +20,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = middle_day(2023);
+        assert_eq!(result, Some(wd::Sun));
     }
 }
