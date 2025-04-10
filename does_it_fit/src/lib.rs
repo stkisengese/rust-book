ac8ac870058129c86177e8d14d950f616ef6ddb2
skisenge <skisengese@gmail.com>
2025-04-10 22:28:36 +0300

    feat: import mod

--- does_it_fit/src/lib.rs
+++ does_it_fit/src/lib.rs
@@ -1,3 +1,6 @@
+pub use crate::areas_volumes::*;
+pub mod areas_volumes;
+
 pub fn area_fit(
 	x: usize,
 	y: usize,
@@ -6,6 +9,14 @@ pub fn area_fit(
 	a: usize,
 	b: usize,
 ) -> bool {
+    let total_area = x * y;
+    let required_area = match objects {
+        GeometricalShapes::Square => areas_volumes::square_area(a) * times,
+        GeometricalShapes::Circle => (areas_volumes::circle_area(a) * times as f64) as usize,
+        GeometricalShapes::Rectangle => areas_volumes::rectangle_area(a, b) * times,
+        GeometricalShapes::Triangle => (areas_volumes::triangle_area(a, b) * times as f64) as usize,
+    };
+    total_area >= required_area
 
 }
 pub fn volume_fit(
@@ -18,7 +29,15 @@ pub fn volume_fit(
 	b: usize,
 	c: usize,
 ) -> bool {
-
+    let total_volume = x * y * z;
+    let required_volume = match objects {
+        GeometricalVolumes::Cube => areas_volumes::cube_volume(a) * times,
+        GeometricalVolumes::Sphere => (areas_volumes::sphere_volume(a) * times as f64) as usize,
+        GeometricalVolumes::Cone => (areas_volumes::cone_volume(a, b) * times as f64) as usize,
+        GeometricalVolumes::Pyramid => (areas_volumes::triangular_pyramid_volume(a as f64, b) * times as f64) as usize,
+        GeometricalVolumes::Parallelepiped => areas_volumes::parallelepiped_volume(a, b, c) * times,
+    };
+    total_volume >= required_volume
 }
 
 #[cfg(test)]
@@ -27,7 +46,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = area_fit(2, 4, GeometricalShapes::Rectangle, 100, 2, 1);
+        assert_eq!(result, true);
     }
 }
