4184b92f3bfba197ba3f3acc38ac91e0194b2086
skisenge <skisengese@gmail.com>
2025-04-11 06:17:05 +0300

    feat: add pattern

--- scores/src/lib.rs
+++ scores/src/lib.rs
@@ -1,6 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// Instructions
+
+// Let's play a little.
+
+// Create a function named score that given a &str, computes the score for that given string as a u64.
+
+// Each letter has a value, you just have to sum the values of the letters in the given string.
+
+// You will need these:
+// Letter 	Value
+// A, E, I, O, U, L, N, R, S, T 	1
+// D, G 	2
+// B, C, M, P 	3
+// F, H, V, W, Y 	4
+// K 	5
+// J, X 	8
+// Q, Z 	10
+
+pub fn score(word: &str) -> u64 {
+    word.chars()
+        .map(|c| match c.to_ascii_uppercase() {
+            'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+            'D' | 'G' => 2,
+            'B' | 'C' | 'M' | 'P' => 3,
+            'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+            'K' => 5,
+            'J' | 'X' => 8,
+            'Q' | 'Z' => 10,
+            _ => 0, // Any other characters are worth 0 points
+        }).sum()
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -8,7 +37,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = score("ThiS is A Test");
+        assert_eq!(result, 14);
     }
 }
