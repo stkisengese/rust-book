5e8b3ec860bcf9e6afb47d5cf3d7217f2f726970
skisenge <skisengese@gmail.com>
2025-04-11 06:42:11 +0300

    feat:  initial commit

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,5 +1,14 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// Instructions
+
+// Create a function named is_pangram which returns true if the given string is a pangram.
+
+// A pangram is a sentence which uses every letter of the alphabet at least once.
+
+// Example: "The quick brown fox jumps over the lazy dog."
+
+pub fn is_pangram(s: &str) -> bool {
+    let lowercase_s = s.to_lowercase();
+    ('a'..='z').all(|c| lowercase_s.contains(c))
 }
 
 #[cfg(test)]
@@ -8,7 +17,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = is_pangram("this is not a pangram!");
+        assert_eq!(result, false);
     }
 }
