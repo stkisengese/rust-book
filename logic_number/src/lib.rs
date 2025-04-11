2ead637d2acba89b9eb353d8d03649ccb788ec0f
skisenge <skisengese@gmail.com>
2025-04-11 22:11:52 +0300

    feat: add number_logic()

--- logic_number/src/lib.rs
+++ logic_number/src/lib.rs
@@ -1,6 +1,25 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn number_logic(num: u32) -> bool {
+    let num_str = num.to_string();
+    let len = num_str.len() as u32;
+    let sum = num_str
+        .chars()
+        .map(|c| c.to_digit(10).unwrap().pow(len))
+        .sum::<u32>();
+    num == sum
 }
+
+
+// pub fn number_logic(num: u32) -> bool {
+//     let digits: Vec<u32> = num
+//         .to_string()
+//         .chars()
+//         .map(|c| c.to_digit(10).unwrap())
+//         .collect();
+
+//     let len = digits.len() as u32;
+
+//     num == digits.iter().map(|d| d.pow(len)).sum()
+// }
 
 #[cfg(test)]
 mod tests {
@@ -8,7 +27,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = number_logic(9);
+        assert_eq!(result, true);
     }
 }
