7bf9b29f19e3de5f5767661d26ea610e4d6ba9ae
skisenge <skisengese@gmail.com>
2025-04-12 13:58:32 +0300

    feat: add scytale_cipher fn

--- scytale_cipher/src/lib.rs
+++ scytale_cipher/src/lib.rs
@@ -1,5 +1,21 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn scytale_cipher(message: String, i: u32) -> String {
+    let size = i as usize;
+    if message.is_empty() || i == 0 {
+        return String::new();
+    }
+
+    let num_colums = (message.len() + size -1) / size;
+    let mut result = String::with_capacity(message.len());
+
+    for col in 0..size {
+        for row in 0..num_colums {
+            let index = row * size + col;
+            if index < message.len() {
+                result.push(message.chars().nth(index).unwrap());
+            }
+        }
+    }
+    result
 }
 
 #[cfg(test)]
@@ -8,7 +24,9 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = scytale_cipher(String::from("scytale Code"), 6);
+        let result2 = scytale_cipher(String::from("scytale Code"), 8);
+        assert_eq!(result, "sec yCtoadle");
+        assert_eq!(result2, "sCcoydtea l e");
     }
 }
