c50d36b11b3c038855e7cca524780289e1e26b34
skisenge <skisengese@gmail.com>
2025-04-12 14:04:18 +0300

    fix padding issue

--- scytale_cipher/src/lib.rs
+++ scytale_cipher/src/lib.rs
@@ -4,17 +4,19 @@ pub fn scytale_cipher(message: String, i: u32) -> String {
         return String::new();
     }
 
-    let num_colums = (message.len() + size -1) / size;
-    let mut result = String::with_capacity(message.len());
+    let chars: Vec<char> = message.chars().collect(); // only once!
+    let num_columns = (chars.len() + size - 1) / size; // ceiling division
+    let mut result = String::with_capacity(chars.len());
 
     for col in 0..size {
-        for row in 0..num_colums {
+        for row in 0..num_columns {
             let index = row * size + col;
-            if index < message.len() {
-                result.push(message.chars().nth(index).unwrap());
+            if index < chars.len() {
+                result.push(chars[index]);
             }
         }
     }
+
     result
 }
 
@@ -27,6 +29,6 @@ mod tests {
         let result = scytale_cipher(String::from("scytale Code"), 6);
         let result2 = scytale_cipher(String::from("scytale Code"), 8);
         assert_eq!(result, "sec yCtoadle");
-        assert_eq!(result2, "sCcoydtea l e");
+        assert_eq!(result2, "sCcoydteale ");
     }
 }
