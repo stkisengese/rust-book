2d7ec1ee53182d5eac00a24a639ca99ddbf603e8
skisenge <skisengese@gmail.com>
2025-04-15 21:57:38 +0300

    feat: add std::rc::c::RC

--- how_many_references/src/lib.rs
+++ how_many_references/src/lib.rs
@@ -1,6 +1,25 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub use std::rc::Rc;
+
+pub struct Node {
+    pub ref_list: Vec<Rc<String>>,
 }
+
+impl Node {
+    pub fn new(ref_list: Vec<Rc<String>>) -> Node {
+        Node { ref_list: ref_list }
+    }
+    pub fn add_element(&mut self, element: Rc<String>) {
+        self.ref_list.push(element);
+    }
+    pub fn rm_all_ref(&mut self, element: Rc<String>) {
+        self.ref_list.retain(|x| !Rc::ptr_eq(x, &element));
+    }
+}
+
+pub fn how_many_references(ref_list: &Rc<String>) -> usize {
+    Rc::strong_count(ref_list)
+}
+
 
 #[cfg(test)]
 mod tests {
