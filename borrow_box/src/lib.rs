95d7ea07ae439f52499eff55b621d10687cf4e0e
skisenge <skisengese@gmail.com>
2025-04-15 16:53:37 +0300

    feat: add Box borrowing for structs

--- borrow_box/src/lib.rs
+++ borrow_box/src/lib.rs
@@ -1,14 +1,47 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct GameSession {
+    pub id: u32,
+    pub p1: (String, u16),
+    pub p2: (String, u16),
+    pub nb_games: u16
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl GameSession {
+    pub fn new(id: u32, p1_name: String, p2_name: String, nb_games: u16) -> Box<GameSession> {
+        let new_game = GameSession {
+            id,
+            p1: (p1_name, 0),
+            p2: (p2_name, 0),
+            nb_games
+        };
+        Box::new(new_game)
+    }
+    pub fn read_winner(&self) -> (String, u16) {
+        let (p1_name, p1_score) = &self.p1;
+        let (p2_name, p2_score) = &self.p2;
+    
+        if p1_score > p2_score {
+            (p1_name.clone(), *p1_score)
+        } else if p2_score > p1_score {
+            (p2_name.clone(), *p2_score)
+        } else {
+            ("Same score! tied".to_string(), 0)
+        }
+        
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    }
+    pub fn update_score(&mut self, user_name: String) {
+        let max_score = (self.nb_games / 2) + 1;
+        if self.p1.1 >= max_score || self.p2.1 >= max_score {
+            return;
+        }
+        if user_name == self.p1.0 {
+            self.p1.1 += 1;
+        } else if user_name == self.p2.0 {
+            self.p2.1 += 1;
+        }
+    }
+    pub fn delete(self) -> String {
+        format!("game deleted: id -> {}", self.id)
     }
 }
