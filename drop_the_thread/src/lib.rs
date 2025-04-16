f3342c343ce259dc687f8e40085538bf943bf527
skisenge <skisengese@gmail.com>
2025-04-16 23:23:31 +0300

    feat: add Interior mutability

--- drop_the_thread/src/lib.rs
+++ drop_the_thread/src/lib.rs
@@ -1,14 +1,68 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub use std::cell::{RefCell, Cell};
+
+#[derive(Debug, Default, Clone, Eq, PartialEq)]
+pub struct Workers {
+    pub drops: Cell<usize>,
+    pub states: RefCell<Vec<bool>>
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Workers {
+    pub fn new() -> Workers {
+        Workers::default()          
+        
+    }
+    pub fn new_worker(&self, c: String) -> (usize, Thread) {
+       let id = self.track_worker();
+       self.states.borrow_mut().push(false);
+       let thread = Thread::new_thread(id, c, self);
+       (id, thread) 
+    }
+    pub fn track_worker(&self) -> usize {
+        self.states.borrow().len()
+    }
+    pub fn is_dropped(&self, id: usize) -> bool {
+        self.states.borrow()[id]
+    }
+    pub fn add_drop(&self, id: usize) {
+        let mut states = self.states.borrow_mut();
+        if states[id] {
+            panic!("{} is already dropped", id);
+        } else {
+            states[id] = true;
+            self.drops.set(self.drops.get() + 1);
+        }
     }
 }
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Thread<'a> {
+    pub pid: usize,
+    pub cmd: String,
+    pub parent: &'a Workers,
+}
+
+impl<'a> Thread<'a> {
+    pub fn new_thread(p: usize, c: String, t: &'a Workers) -> Self {
+        Thread { pid: p, cmd: c, parent: t }
+    }
+    pub fn skill(self) {
+        drop(self);
+    }
+}
+
+impl<'a> Drop for Thread<'a> {
+    fn drop(&mut self) {
+        self.parent.add_drop(self.pid);
+    }
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = (2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
