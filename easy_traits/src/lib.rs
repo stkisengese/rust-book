aad62bf5f9ae63e1b5bd8ff06de85c5028bfb0a6
skisenge <skisengese@gmail.com>
2025-04-19 15:14:53 +0300

    feat: add self.clone()

--- easy_traits/src/lib.rs
+++ easy_traits/src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Clone)]
+pub struct StringValue {
+    pub value: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait AppendStr {
+    fn append_str(&mut self, str_to_append: String) -> Self;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn append_number(&mut self, nb_to_append: f64) -> Self;
+
+    fn remove_punctuation_marks(&mut self) -> Self;
+}
+
+impl AppendStr for StringValue {
+    fn append_str(&mut self, str_to_append: String) -> Self {
+        self.value.push_str(&str_to_append);
+        self.clone()
+    }
+
+    fn append_number(&mut self, nb_to_append: f64) -> Self {
+        self.value.push_str(&nb_to_append.to_string());
+        self.clone()
+    }
+
+    fn remove_punctuation_marks(&mut self) -> Self {
+        // self.value.retain(|c| !c.is_ascii_punctuation());
+        // self.value = self.value.chars().filter(|c| !c.is_ascii_punctuation()).collect();
+        // self.value = self.value.replace(|c: char| c.is_ascii_punctuation(), "");
+        self.value.retain(|c| !matches!(c, '!' | '?' | '.' | ',' | ';' | ':' | '\'' | '"' | '(' | ')' | '[' | ']' | '{' | '}' | '<' | '>' | '/' | '\\' | '|' | '@' | '#' | '$' | '%' | '^' | '&' | '*' | '+' | '-' | '=' | '`' | '~'));
+        self.clone()
     }
 }
