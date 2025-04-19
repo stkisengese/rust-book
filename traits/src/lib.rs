01f076fdfc2b25e7f6e204964788002c98d9e58b
skisenge <skisengese@gmail.com>
2025-04-19 19:07:31 +0300

    feat: add traits n generecs

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -1,14 +1,52 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Player {
+	pub name: String,
+	pub strength: f64,
+	pub score: i32,
+	pub money: i32,
+	pub weapons: Vec<String>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct Fruit {
+	pub weight_in_kg: f64,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct Meat {
+	pub weight_in_kg: f64,
+	pub fat_content: f64,
+}
+
+impl Player {
+	pub fn eat<T: Food> (&mut self, food: T) {
+		self.strength += food.gives();
+	}
+}
+
+pub trait Food {
+	fn gives(&self) -> f64;
+}
+
+impl Food for Fruit {
+    fn gives(&self) -> f64 {
+        self.weight_in_kg * 4.0
+    }
+}
+
+impl Food for Meat {
+    fn gives(&self) -> f64 {
+        let fat_weight = self.weight_in_kg * self.fat_content;
+        let protein_weight = self.weight_in_kg - fat_weight;
+        (fat_weight * 9.0) + (protein_weight * 4.0)
+    }
+}
+
+impl fmt::Display for Player {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "{}", self.name)?;
+        writeln!(f, "Strength: {}, Score: {}, Money: {}",
+            self.strength, self.score, self.money)?;
+        writeln!(f, "Weapons: {:?}", self.weapons)
     }
 }
