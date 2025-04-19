6943de5274e24144ad2202865dbd80faafbc4738
skisenge <skisengese@gmail.com>
2025-04-20 01:27:06 +0300

    feat:  add chrono n json crates

--- commits_stats/src/lib.rs
+++ commits_stats/src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::collections::HashMap;
+use chrono::{DateTime, Datelike, Utc};
+use json::JsonValue;
+
+
+pub fn commits_per_author(data: &JsonValue) -> HashMap<String, u32> {
+    let mut map = HashMap::new();
+
+    for commit in data.members() {
+        if let Some(author) = commit["author"]["login"].as_str() {
+            *map.entry(author.to_string()).or_insert(0) += 1;
+        }
+    }
+
+    map
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn commits_per_week(data: &JsonValue) -> HashMap<String, u32> {
+    let mut map = HashMap::new();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    for commit in data.members() {
+        if let Some(date_str) = commit["commit"]["author"]["date"].as_str() {
+            if let Ok(date) = DateTime::parse_from_rfc3339(date_str) {
+                let date_utc = date.with_timezone(&Utc);
+                let iso_week = date_utc.iso_week();
+                let week_str = format!("{}-W{}", iso_week.year(), iso_week.week());
+                *map.entry(week_str).or_insert(0) += 1;
+            }
+        }
     }
+    map
 }
