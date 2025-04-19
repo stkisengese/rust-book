3356af3f95b3d91496e474b980c2df1e66b5d56b
skisenge <skisengese@gmail.com>
2025-04-19 16:24:09 +0300

    fix: -ve err

--- easy_traits/src/lib.rs
+++ easy_traits/src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub struct StringValue {
     pub value: String,
 }
@@ -12,21 +12,23 @@ pub trait AppendStr {
 }
 
 impl AppendStr for StringValue {
-    fn append_str(&mut self, str_to_append: String) -> Self {
+
+    fn append_str(&mut self, str_to_append: String) -> Self{
         self.value.push_str(&str_to_append);
         self.clone()
     }
 
-    fn append_number(&mut self, nb_to_append: f64) -> Self {
+    fn append_number(&mut self, nb_to_append: f64) -> Self{
         self.value.push_str(&nb_to_append.to_string());
         self.clone()
     }
 
-    fn remove_punctuation_marks(&mut self) -> Self {
-        // self.value.retain(|c| !c.is_ascii_punctuation());
-        // self.value = self.value.chars().filter(|c| !c.is_ascii_punctuation()).collect();
-        // self.value = self.value.replace(|c: char| c.is_ascii_punctuation(), "");
-        self.value.retain(|c| !matches!(c, '!' | '?' | '.' | ',' | ';' | ':' | '\'' | '"' | '(' | ')' | '[' | ']' | '{' | '}' | '<' | '>' | '/' | '\\' | '|' | '@' | '#' | '$' | '%' | '^' | '&' | '*' | '+' | '-' | '=' | '`' | '~'));
-        self.clone()
+    fn remove_punctuation_marks(&mut self) -> Self{
+        self.value = self
+            .value
+            .chars()
+            .filter(|c| !['.', ',', '?', '!'].contains(c))
+            .collect();
+        self.clone()    
     }
 }
