c5b41ad4c13b55007a4d3f6ad9edf9869e4b6667
skisenge <skisengese@gmail.com>
2025-04-19 13:20:19 +0300

    feat: add struct generics

--- generics_list/src/lib.rs
+++ generics_list/src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug, Clone)]
+pub struct List<T> {
+    pub head: Option<Box<Node<T>>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Clone)]
+pub struct Node<T> {
+    pub value: T,
+    pub next: Option<Box<Node<T>>>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<T> List<T> {
+    pub fn new() -> List<T> {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, value: T) {
+        self.head = Some(Box::new(Node {
+            value,
+            next: self.head.take(),
+        }));
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.head.is_none() {
+            return None;
+        }
+
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.value
+        })
+    }
+    
+    pub fn len(&self) -> usize {
+        let mut count = 0;
+        let mut current = &self.head;
+        while let Some(node) = current {
+            count += 1;
+            current = &node.next;
+        }
+        count
     }
 }
