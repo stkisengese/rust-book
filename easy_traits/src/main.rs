e1022344e45d5643374f7ba96a21f337e06a230e
skisenge <skisengese@gmail.com>
2025-04-19 16:20:05 +0300

    fix: again -ve err

--- easy_traits/src/main.rs
+++ easy_traits/src/main.rs
@@ -2,13 +2,20 @@ use easy_traits::*;
 
 fn main() {
     let mut str_aux = StringValue {
-        value: String::from("hello"),
+        value: String::from(""),
     };
+    str_aux.append_number(-1.0);
+    println!("After append: {}", str_aux.value);
+
+
+    
 
     println!("Before append: {}", str_aux.value);
 
     str_aux.append_str(String::from(" there!"));
     println!("After append: {}", str_aux.value);
+
+
 
     str_aux.remove_punctuation_marks();
     println!("After removing punctuation: {}", str_aux.value);
