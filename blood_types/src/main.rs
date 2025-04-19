c7521feeca2769bfc0c84a062883745e4646d140
skisenge <skisengese@gmail.com>
2025-04-19 19:15:37 +0300

    feat: add main fn

---
+++ blood_types/src/main.rs
@@ -0,0 +1,20 @@
+use blood_types::*;
+
+fn main() {
+	let blood_type: BloodType = "O+".parse().unwrap();
+	println!("recipients of O+ {:?}", blood_type.recipients());
+	println!("donors of O+ {:?}", blood_type.donors());
+	let another_blood_type: BloodType = "A-".parse().unwrap();
+	println!(
+		"donors of O+ can receive from {:?} {:?}",
+		&another_blood_type,
+		blood_type.can_receive_from(&another_blood_type)
+	);
+}
+
+// $ cargo run
+// recipients of O+ [AB+, O+, A+, B+]
+// donors of O+ [O+, O-]
+// donors of O+ can receive from A- false
+// $
+// 
