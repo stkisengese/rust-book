b6c94f041c5b59c576b37996984d2d65a2cc6721
skisenge <skisengese@gmail.com>
2025-04-19 11:38:28 +0300

    feat: add generics

--- generics/src/lib.rs
+++ generics/src/lib.rs
@@ -1,5 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn identity<T>(v: T) -> T {
+    v
 }
 
 #[cfg(test)]
@@ -8,7 +8,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = identity("Hello, world!");
+        assert_eq!(result, "Hello, world!");
+        assert_eq!(identity(3), 3);
     }
 }
