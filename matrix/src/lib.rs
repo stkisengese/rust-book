8228bf3f439cc19878b0fec1c3fa3ab4dcfa5768
skisenge <skisengese@gmail.com>
2025-04-23 00:19:10 +0300

    feat: add matrix

--- matrix/src/lib.rs
+++ matrix/src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use lalgebra_scalar::Scalar;
+
+#[derive(Default, Debug, Clone, Eq, PartialEq)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+
+impl <T: Scalar<Item = T> + Default> Matrix<T> {
+	pub fn new() -> Matrix<T> {
+        Matrix(vec![vec![T::default()]])
+	}
+
+	pub fn zero(row: usize, col: usize) -> Matrix<T> {
+        Matrix(vec![vec![T::zero(); col]; row])
+	}
+
+	pub fn identity(n: usize) -> Matrix<T> {
+        let mut identity_matrix = vec![vec![T::default(); n]; n];
+        for i in 0..n {
+            identity_matrix[i][i] = T::one(); // Set diagonal elements to 1
+        }
+        Matrix(identity_matrix)
+	}
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//     #[test]
+//     fn test_matrix () {
+//         let m: Matrix<u32> = Matrix(vec![vec![0, 0, 0, 0], vec![0, 0, 0, 0], vec![0, 0, 0, 0]]);
+//         let result1 = Matrix::<i32>::identity(4);
+//         let result2 = Matrix::<f64>::zero(3, 4);
+//         assert_eq!(m, Matrix([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]));
+//         assert_eq!(result1, Matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]));
+//         // assert_eq!(result2, Matrix([[0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0]]));
+//     }
+// }
