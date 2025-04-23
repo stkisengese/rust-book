6d3c2dd819a279b78c92ade373b02bece497d275
skisenge <skisengese@gmail.com>
2025-04-23 03:31:36 +0300

    feat: add traits in lifetime structs

--- border_cross/src/lib.rs
+++ border_cross/src/lib.rs
@@ -1,14 +1,70 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[allow(dead_code)]
+pub struct Car<'a> {
+	pub plate_nbr: &'a str,
+	pub model: &'a str,
+	pub horse_power: u32,
+	pub year: u32,
 }
+
+#[allow(dead_code)]
+pub struct Truck<'a> {
+	pub plate_nbr: &'a str,
+	pub model: &'a str,
+	pub horse_power: u32,
+	pub year: u32,
+	pub load_tons: u32,
+}
+
+pub trait Vehicle {
+	fn model(&self) -> &str;
+	fn year(&self) -> u32;
+}
+
+impl Vehicle for Truck<'a> {
+    fn model(&self) -> &str {
+        self.model
+    }
+    fn year(&self) -> u32 {
+        self.year
+    }
+}
+
+impl Vehicle for Car<'a> {
+    fn model(&self) -> &str {
+        self.model
+    }
+    fn year(&self) -> u32 {
+        self.year
+    }
+}
+
+fn all_models(list: Vec<&Vehicle>) -> Vec<&str> {
+    list.iter().map(|v| v.model()).collect()
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn boarder_cross_test() {
+        let vehicles = vec![
+            &Car {
+                plate_nbr: "A3D5C7",
+                model: "Model 3",
+                horse_power: 325,
+                year: 2010,
+            },
+            &Truck {
+                plate_nbr: "V3D5CT",
+                model: "Ranger",
+                horse_power: 325,
+                year: 2010,
+                load_tons: 40,
+            },
+        ];
+        let result = all_models(vehicles);
+        assert_eq!(result, ["Model 3", "Ranger"]);
     }
 }
