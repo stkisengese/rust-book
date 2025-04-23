bd3cdcb62b95969b90ae0c3b5f913941d4b5c886
skisenge <skisengese@gmail.com>
2025-04-23 03:13:22 +0300

    feat: add strip_prefix()

--- delete_prefix/src/lib.rs
+++ delete_prefix/src/lib.rs
@@ -1,5 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn delete_prefix<'a>(prefix: &'a str, s: &'a str) -> Option<&'a str> {
+    // if s.starts_with(prefix) {
+    //     Some(&s[prefix.len()..])
+    // } else {
+    //     None
+    // }
+    s.strip_prefix(prefix)
 }
 
 #[cfg(test)]
@@ -7,8 +12,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_delete_prefix() {
+        let result = delete_prefix("foo", "foobar");
+        assert_eq!(result, Some("bar"));
     }
 }
