89ebc969cc4c4bbdd6e62b86f1da3185762141c5
skisenge <skisengese@gmail.com>
2025-04-26 01:31:59 +0300

    feat: add closures n iterators

--- closures/src/lib.rs
+++ closures/src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn first_fifty_even_square() -> Vec<i32> {
+    (2..).step_by(2).map(|x| x * x).take(50).collect()
 }
