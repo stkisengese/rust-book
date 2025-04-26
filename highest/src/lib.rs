a1f7b3688eb2e8887a6726851454139926171608
skisenge <skisengese@gmail.com>
2025-04-26 04:52:52 +0300

    feat: add struct methods

--- highest/src/lib.rs
+++ highest/src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug)]
+pub struct Numbers<'a> {
+    numbers: &'a [u32],
 }
 
+impl<'a> Numbers<'a> {
+    pub fn new(numbers: &'a [u32]) -> Self {
+        Numbers { numbers }
+    }
+
+    pub fn list(&self) -> &[u32] {
+        self.numbers
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.numbers.last().copied()
+    }
+
+    pub fn highest(&self) -> Option<u32> {
+        self.numbers.iter().max().copied()
+    }
+
+    pub fn highest_three(&self) -> Vec<u32> {
+        let mut sorted: Vec<u32> = self.numbers.to_vec();
+        sorted.sort_unstable_by(|a, b| b.cmp(a)); // Sort descending
+        sorted.truncate(3); // Keep only top 3
+        sorted
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_numbers() {
+        let expected = [30, 500, 20, 70];
+        let n = Numbers::new(&expected);
+        assert_eq!(n.list(), &expected);
+        assert_eq!(n.highest(), Some(500));
+        assert_eq!(n.latest(), Some(70));
+        assert_eq!(n.highest_three(), vec![500, 70, 30]);
     }
 }
