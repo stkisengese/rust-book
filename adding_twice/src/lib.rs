f7fec430d70147c339171491c91879e72f060931
skisenge <skisengese@gmail.com>
2025-04-26 03:50:26 +0300

    feat: add closures as arguments

--- adding_twice/src/lib.rs
+++ adding_twice/src/lib.rs
@@ -1,5 +1,12 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn add_curry(x: i32) -> impl Fn(i32) -> i32 {
+    move |y| x + y
+}
+
+pub fn twice<T>(f: T) -> impl Fn(i32) -> i32
+where
+    T: Fn(i32) -> i32,
+{
+    move |x| f(f(x))
 }
 
 #[cfg(test)]
@@ -7,8 +14,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_add_curry_positive() {
+        let add5 = add_curry(5);
+        assert_eq!(add5(10), 15);
+        assert_eq!(add5(0), 5);
+        assert_eq!(add5(-5), 0);
     }
-}
+
+    #[test]
+    fn test_add_curry_negative() {
+        let sub3 = add_curry(-3);
+            let add5 = add_curry(5);
+            let add10_twice = twice(add5);
+            assert_eq!(add10_twice(3), 13);
+        }
+    }
+
+    #[test]
+    fn test_add_curry_zero() {
+        let add0 = add_curry(0);
+        assert_eq!(add0(10), 10);
+        assert_eq!(add0(-5), -5);
+        assert_eq!(add0(0), 0);
+    }
