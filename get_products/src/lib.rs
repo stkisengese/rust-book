becf4c58359550e6f0141088901e5e64bb3f91fd
skisenge <skisengese@gmail.com>
2025-04-26 04:15:20 +0300

    feat: add double iteration

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -1,5 +1,14 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
+    arr.iter()
+       .enumerate()
+       .map(|(i, _)| {
+           arr.iter()
+              .enumerate()
+              .filter(|&(j, _)| j != i)
+              .map(|(_, &x)| x)
+              .product()
+       })
+       .collect()
 }
 
 #[cfg(test)]
@@ -7,8 +16,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_get_products() {
+        let arr = vec![1, 7, 3, 4];
+        let expected = vec![84, 12, 28, 21];
+        assert_eq!(get_products(arr), expected);
     }
 }
