5d019fd922d15a0ba69bc59d9a606d3218502aa2
skisenge <skisengese@gmail.com>
2025-04-26 17:11:04 +0300

    feat: add iterator ops  and cmp

--- step_iterator/src/lib.rs
+++ step_iterator/src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::ops::Add;
+use std::cmp::PartialOrd;
+
+pub struct StepIterator<T> {
+    current: T,
+    end: T,
+    step: T,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<T> StepIterator<T>
+where
+    T: Add<Output = T> + PartialOrd + Copy,
+{
+    pub fn new(beg: T, end: T, step: T) -> Self {
+        StepIterator {
+            current: beg,
+            end,
+            step,
+        }
     }
 }
+
+impl<T> Iterator for StepIterator<T>
+where
+    T: Add<Output = T> + PartialOrd + Copy,
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current > self.end {
+            return None; 
+        }
+
+        let result = self.current;
+        self.current = self.current + self.step;
+
+        Some(result)
+    }
+}
+
