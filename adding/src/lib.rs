9e5b419156e3e991bf42c033df88ea1b87ca2567
skisenge <skisengese@gmail.com>
2025-04-26 03:39:15 +0300

    feat: add move for ownership in closures

--- adding/src/lib.rs
+++ adding/src/lib.rs
@@ -1,5 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn add_curry(x: i32) -> impl Fn(i32) -> i32 {
+    move |y| x + y
 }
 
 #[cfg(test)]
@@ -7,8 +7,50 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_add_curry_positive() {
+        let add5 = add_curry(5);
+        assert_eq!(add5(10), 15);
+        assert_eq!(add5(0), 5);
+        assert_eq!(add5(-5), 0);
+    }
+
+    #[test]
+    fn test_add_curry_negative() {
+        let sub3 = add_curry(-3);
+        assert_eq!(sub3(10), 7);
+        assert_eq!(sub3(0), -3);
+        assert_eq!(sub3(3), 0);
+    }
+
+    #[test]
+    fn test_add_curry_zero() {
+        let add0 = add_curry(0);
+        assert_eq!(add0(10), 10);
+        assert_eq!(add0(-5), -5);
+        assert_eq!(add0(0), 0);
+    }
+
+    #[test]
+    fn test_add_curry_large_numbers() {
+        let add_million = add_curry(1_000_000);
+        assert_eq!(add_million(1), 1_000_001);
+        assert_eq!(add_million(-1_000_000), 0);
+    }
+
+    #[test]
+    fn test_multiple_instances() {
+        let add10 = add_curry(10);
+        let add20 = add_curry(20);
+        
+        assert_eq!(add10(5), 15);
+        assert_eq!(add20(5), 25);
+        assert_eq!(add10(add20(5)), 35); // Chaining
+    }
+
+    #[test]
+    fn test_closure_reuse() {
+        let add7 = add_curry(7);
+        assert_eq!(add7(3), 10);
+        assert_eq!(add7(10), 17); // Same closure reused
     }
 }
