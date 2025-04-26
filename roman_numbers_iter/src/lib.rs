81c634c824520647cf719217a1f35711aeeecd4a
skisenge <skisengese@gmail.com>
2025-04-26 16:42:36 +0300

    fix: fix next va;lue

--- roman_numbers_iter/src/lib.rs
+++ roman_numbers_iter/src/lib.rs
@@ -1,107 +1,108 @@
-pub mod roman_digit {
-    #[derive(Copy, Clone, Debug, PartialEq, Eq)]
-    pub enum RomanDigit {
-	    Nulla,
-	    I,
-	    V,
-	    X,
-	    L,
-	    C,
-	    D,
-	    M,
-    }
+use crate::RomanDigit::*;
 
-    impl From<u32> for RomanDigit {
-        fn from(value: u32) -> Self {
-            match value {
-                0 => RomanDigit::Nulla,
-                1 => RomanDigit::I,
-                5 => RomanDigit::V,
-                10 => RomanDigit::X,
-                50 => RomanDigit::L,
-                100 => RomanDigit::C,
-                500 => RomanDigit::D,
-                1000 => RomanDigit::M,
-                _ => panic!("Invalid value"),
-            }
-        }
-    }
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum RomanDigit {
+    Nulla,
+    I,
+    V,
+    X,
+    L,
+    C,
+    D,
+    M,
 }
 
-pub mod roman_number {
-    use super::roman_digit::RomanDigit;
-    use RomanDigit::*;
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct RomanNumber(pub Vec<RomanDigit>);
 
-    #[derive(Clone, Debug, PartialEq, Eq)]
-    pub struct RomanNumber(pub Vec<RomanDigit>);
+impl From<u32> for RomanNumber {
+    fn from(value: u32) -> Self {
+        let mapping = [
+            (1000, vec![M]),
+            (900, vec![C, M]),
+            (500, vec![D]),
+            (400, vec![C, D]),
+            (100, vec![C]),
+            (90, vec![X, C]),
+            (50, vec![L]),
+            (40, vec![X, L]),
+            (10, vec![X]),
+            (9, vec![I, X]),
+            (5, vec![V]),
+            (4, vec![I, V]),
+            (1, vec![I]),
+        ];
 
-    
-    impl From<u32> for RomanNumber {
-        fn from(value: u32) -> Self {
-            if value == 0 {
-                return RomanNumber(vec![RomanDigit::Nulla]);
-            }
+        let mut digits = Vec::new();
+        let mut value1 = value;
 
-            let mut value1 = value;
-            
-            let mut digits = Vec::new();
-            let mapping = [
-                (1000, vec![M]),
-                (900, vec![C, M]),
-                (500, vec![D]),
-                (400, vec![C, D]),
-                (100, vec![C]),
-                (90, vec![X, C]),
-                (50, vec![L]),
-                (40, vec![X, L]),
-                (10, vec![X]),
-                (9, vec![I, X]),
-                (5, vec![V]),
-                (4, vec![I, V]),
-                (1, vec![I]),
-            ];
-
+        if value == 0 {
+            digits.push(Nulla);
+        } else {
             for &(num, ref roman) in mapping.iter() {
-                while *&value1 >= num {
+                while value1 >= num {
                     digits.extend(roman.clone());
                     value1 -= num;
                 }
             }
-
-            RomanNumber(digits)
         }
-    }
 
-    impl Iterator for RomanNumber {
-        type Item = RomanNumber;
-        
-        fn next(&mut self) -> Option<Self::Item> {
-            let current_value = match self.0.as_slice() {
-                [RomanDigit::Nulla] => 0,
-                _ => 1,
-            };
-
-            let result = if current_value == 0 {
-                RomanNumber(vec![RomanDigit::I])
-            } else {
-                let mut next_digit = self.0.clone();
-                next_digit.push(RomanDigit::I);
-                RomanNumber(next_digit)
-            };
-
-            Some(result)
-        }
+        RomanNumber(digits)
     }
 }
 
-pub use roman_digit::RomanDigit;
-pub use roman_number::RomanNumber;
+// Implement the Iterator trait for RomanNumber
+impl Iterator for RomanNumber {
+    type Item = RomanNumber;
 
+    fn next(&mut self) -> Option<Self::Item> {
+        // Increment the value represented by the Roman numeral
+        let current_value = RomanNumber::to_u32(&self.0) + 1;
+
+        // Update the digits to reflect the new value
+        let next_roman = RomanNumber::from(current_value);
+        self.0 = next_roman.0.clone();
+
+        Some(RomanNumber(self.0.clone()))
+    }
+}
+
+impl RomanNumber {
+    // Helper function to convert RomanNumber back to u32
+    fn to_u32(digits: &[RomanDigit]) -> u32 {
+        let mapping = [
+            (M, 1000),
+            (D, 500),
+            (C, 100),
+            (L, 50),
+            (X, 10),
+            (V, 5),
+            (I, 1),
+            (Nulla, 0),
+        ];
+
+        let mut total = 0;
+        let mut prev_value = 0;
+
+        for digit in digits.iter().rev() {
+            let value = mapping.iter().find(|(d, _)| *d == *digit).unwrap().1;
+
+            if value < prev_value {
+                total -= value;
+            } else {
+                total += value;
+            }
+
+            prev_value = value;
+        }
+
+        total
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use super::roman_digit::RomanDigit::*;
-    use super::roman_number::RomanNumber;
+   use super::*;
 
     #[test]
     fn test_roman_number() {
@@ -116,8 +117,8 @@ mod tests {
 
     #[test]
     fn test_roman_number_iterator() {
-        let mut number = RomanNumber::from(Nulla as u32);
-        assert_eq!(number, RomanNumber(vec![Nulla]));
-        assert_eq!(number.next(), Some(RomanNumber(vec![I])));
+        let mut number = RomanNumber::from(8);
+        assert_eq!(number, RomanNumber(vec![V, I, I, I]));
+        assert_eq!(number.next(), Some(RomanNumber(vec![I, X])));
     }
 }
